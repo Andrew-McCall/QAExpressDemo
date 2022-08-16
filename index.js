f39b77bfc3491800cc6ed2fe37d5ea013775ac9b
Andrew-McCall <amccall@qa.com>
2022-08-16 12:22:10 +0100

    Renamed Var

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 })
 
 /// Error Middlewear (LAST!)
-app.use((Error, req, res, next)=> {
+app.use((err, req, res, next)=> {
     console.log("Error")
     res.status(500).send('Mistakes were made');
     next()
